c96dadd7296ab9ae5ee544f5bb71c5874a6b417e
yagami229 <ilushkkins1226@tutamail.com>
2025-12-01 19:01:06 +0300

    Update is.even.py
    name main...

--- is.even.py
+++ is.even.py
@@ -1,6 +1,11 @@
 import math
-Z = int(input('Введите целое число: '))
-def parity(Z):
-    if Z % 2 == 0: print('Число четное')
-    elif Z % 2 == 1: print('Число нечетное')
-parity(Z)
+
+def even():
+    Z = int(input('Введите целое число: '))
+    def parity(Z):
+        if Z % 2 == 0: print('Число четное')
+        elif Z % 2 == 1: print('Число нечетное')
+    print(parity(Z))
+if __name__== '__main__':
+    even()
+    

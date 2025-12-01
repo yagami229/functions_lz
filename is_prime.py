a7275859ad5a47d4924fb90edcf1c9e5bd65c325
yagami229 <ilushkkins1226@tutamail.com>
2025-12-01 19:01:58 +0300

    Update is_prime.py
    name main...

--- is_prime.py
+++ is_prime.py
@@ -1,7 +1,10 @@
-p = int(input('Введите число: '))
-def simple(p):
-    d = 2
-    while p % d != 0:
-        d +=  1 
-    return d == p
-print(simple(p))
+def prime():
+    p = int(input('Введите число: '))
+    def simple(p):
+        d = 2
+        while p % d != 0:
+            d +=  1 
+        return d == p
+    print(simple(p))
+if __name__== '__main__':
+    prime()

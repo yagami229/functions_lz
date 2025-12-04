904fc7002c39839f77b838fc9e1ed875e3766dd2
yagami229 <ilushkkins1226@tutamail.com>
2025-12-04 21:47:49 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import temperature
+import is_even
+import is_prime
+import circle
+def main():
+      circle.main()
+      is_even.main()
+      temperature.main()
+      is_prime.main()
+if __name__ == "__main__":
+        main()
